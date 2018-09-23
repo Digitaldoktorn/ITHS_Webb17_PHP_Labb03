23e5ead1fdd5e69608d5baf524752c8287815690
Anders Eriksson <anderse@unseen.is>
2018-09-23 20:23:49 +0200

    htaccess + post

--- fiskbil.php
+++ fiskbil.php
@@ -1,6 +1,7 @@
 <?php
     // Fixing å, ä, ö in the result
     // header('Content-Type: application/json');
+    // lyckas ej med htacess rewrite-kolla med Hans
 
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
@@ -29,44 +30,66 @@
         $limit = " LIMIT " . $_GET['limit'];
     }
 
+    // Post data
+    if(isset($_POST['fisksort'])) {
+        $stmt = $dbh->prepare("
+            INSERT INTO Fisk
+            (fisksort, pris, fetthalt, zon)
+            VALUES
+            (:fisksort, :pris, :fetthalt, :zon)
+        ");
 
-    // Checking if id is set and is not NULL
-    if(isset($_GET['id'])) {
+        $stmt->bindParam(":fisksort", $_POST['fisksort']);
+        $stmt->bindParam(":pris", $_POST['pris']);
+        $stmt->bindParam(":fetthalt", $_POST['fetthalt']);
+        $stmt->bindParam(":zon", $_POST['zon']);
+
+        if($stmt->execute()){
+            $result = $dbh->lastInsertId();
+        }
+        else {
+            $result = $stmt->errorInfo();
+        }
+        $result = json_encode($result);
+        echo $result;
+
+    }
+    else {
         // Checking for numeric id value
         if(is_numeric($_GET["id"])){
             $idString = "Fisk.id = " . $_GET["id"];
         }
+
+
+        // Preparing SQL query
+        $stmt = $dbh->prepare("
+            SELECT *
+            FROM Fisk
+            WHERE " . $idString .
+            $sort . 
+            $limit
+        
+        );
+
+        // Fetching the result into associative array
+        if($stmt->execute()){
+            $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        } 
         else {
-            echo "Fel! ID måste vara ett nummer.";
-            exit;
+            $result = $stmt->errorInfo();
+            var_dump($stmt);
         }
+
     }
-
-    // Preparing SQL query
-    $stmt = $dbh->prepare("
-        SELECT *
-        FROM Fisk, Färdigmat
-        WHERE " . $idString .
-        $sort . 
-        $limit
-    
-    );
-
-    // Fetching the result into associative array
-    if($stmt->execute()){
-        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
-
-    } 
-    else {
-        $result = $stmt->errorInfo();
-        var_dump($stmt);
-    }
-
-    if($result == NULL){
-        echo "Fel! Värde saknas.";
+    if(is_null($result)){
+        $result = [
+            "DEt finns ingen fisk som matchar din sökning. Försök igen." . $_GET['id'] . $_GET['title']
+        ];
     }
 
     $result = json_encode($result);
     echo $result;
     echo "<br>";
+
 ?>
