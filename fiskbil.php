f82ce50194dbec002cefaa20b42abb3b320bee13
Anders Eriksson <anderse@unseen.is>
2018-09-23 15:46:06 +0200

    limit

--- fiskbil.php
+++ fiskbil.php
@@ -1,5 +1,5 @@
 <?php
-    // fixing å, ä, ö in the result
+    // Fixing å, ä, ö in the result
     // header('Content-Type: application/json');
 
     ini_set('display_errors', 1);
@@ -8,9 +8,12 @@
 
     $idString = "1";
     $sort = "";
+    $limit = "";
 
+    // Connecting to database
     $dbh = new PDO("mysql:host=localhost; dbname=Fiskbil_labb3_php; charset=utf8", "Anders", "abc123");
 
+    // Ascending and descending ordering
     if(isset($_GET['sort'])){
         $sort = " ORDER BY " . $_GET['sort'];
         if(isset($_GET['DESC'])){
@@ -21,7 +24,15 @@
         }
     }
 
+    // Limiting result
+    if((isset($_GET['limit'])) && (is_numeric($_GET['limit']))){
+        $limit = " LIMIT " . $_GET['limit'];
+    }
+
+
+    // Checking if id is set and is not NULL
     if(isset($_GET['id'])) {
+        // Checking for numeric id value
         if(is_numeric($_GET["id"])){
             $idString = "Fisk.id = " . $_GET["id"];
         }
@@ -31,14 +42,17 @@
         }
     }
 
+    // Preparing SQL query
     $stmt = $dbh->prepare("
         SELECT *
-        FROM Fisk
+        FROM Fisk, Färdigmat
         WHERE " . $idString .
-        $sort
+        $sort . 
+        $limit
     
     );
 
+    // Fetching the result into associative array
     if($stmt->execute()){
         $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
